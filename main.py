425e6c2cb378189d8d81cfef91ef4c8bce0c9f91
cincottash <cincotta@udel.edu>
2020-01-31 21:32:40 -0500

    Setup canvas

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import pygame
 from globals import *
-
+import random
 pygame.init()
 
-background = pygame.image.load("assets/background.jpg")
+#background = pygame.image.load("assets/background.jpg")
 
-canvas = pygame.display.set_mode((canvasWidth,canvasHeight))
 #pygame.display.set_caption('Space')
 
 def main():
 
 	while endlessLoop:
-		#reset the background
-		canvas.blit(background, (0,0))
-			
 
-		#"Commit" the changes
-		pygame.display.update()
+		arr=[]
+		RED=(255,10,10)
+		BLU=(10,255,10)
+		GRN=(10,10,255)
+		arr.append(RED)
+		arr.append(BLU)
+		arr.append(GRN)
+		SCREEN_X=400
+		SCREEN_Y=400
+
+		screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
+		square=pygame.Surface((1, 1))
+
+		for j in range (SCREEN_Y):
+		    for i in range (SCREEN_X):
+		        num=random.randint(0,2)
+		        square.fill(arr[num])
+		        draw_me=pygame.Rect((j+1), (i+1), 1, 1)
+		        screen.blit(square,draw_me)
+		pygame.display.flip()
 
 
 
