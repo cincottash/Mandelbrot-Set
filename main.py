5eb7ebb9808d30dd2f692eda719f34237fe78682
cincottash <cincotta@udel.edu>
2020-01-31 20:47:11 -0500

    Setup canvas

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import pygame
+
+pygame.init()
+
+canvasWidth = 1920
+canvasHeight = 1080
+
+background = pygame.image.load("assets/background.jpg")
+
+canvas = pygame.display.set_mode((canvasWidth,canvasHeight))
+#pygame.display.set_caption('Space')
+
+endlessLoop = 1
+
+def main():
+
+	while endlessLoop:
+		#reset the background
+		canvas.blit(background, (0,0))
+			
+
+		#"Commit" the changes
+		pygame.display.update()
+
+
+
+if __name__ == '__main__':
+	main()
