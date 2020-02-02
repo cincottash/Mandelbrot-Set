9bc566df3a44c6dd5c8f7a931bbfd7c48196fde5
cincottash <cincotta@udel.edu>
2020-02-02 01:15:00 -0500

    Minor changes

--- main.py
+++ main.py
@@ -1,48 +1,22 @@
-from pylab import imshow,show,gray
-from numpy import zeros,linspace
-import pygame
 import time
 import random
-
-#math: Zn+1 = Zn + c
-#c = a + bi
-#c^2 = a^2 + 2abi - b^2
-#Ex: Z0 = c
-#Ex: Z1 = c^2 + c
-#Ex: Z2 = (c^2 + c)^2 + c
-
-pygame.init()
-canvasWidth = 400
-canvasHeight = 400
-
+from initializeThreads import *
 
 def main():
-	screen = pygame.display.set_mode((canvasWidth, canvasHeight))
-	n= 400
 
-	M = zeros([n,n],int)
-	xvalues = linspace(-2,2,n)
-	yvalues = linspace(-2,2,n)
+	start_time = time.time()
 
+	for numThread in threadList:
+		numThread.start()
 
-	for u,x in enumerate(xvalues):
-		for v,y in enumerate(yvalues):
-			z = 0 + 0j
-			c = complex(x,y)
-			for i in range(100):
-				z = z*z + c
-				if abs(z) > 2.0:
-					# print(int(255/((abs(z) % 3) + 1)))
-					screen.set_at((u, v), ( , , ))
-					break
-				screen.set_at((u, v), (0,0,0))
-	pygame.display.update()
-	time.sleep(100)
+	for numThread in threadList:
+		numThread.join()
+	
+	print("--- Completed in %s seconds with %d threads running ---" % (time.time() - start_time, len(threadList)))
 
-
-
-
-
+	while True:
+		pygame.display.update()
 
 if __name__ == '__main__':
+	initializeThreads()
 	main()
